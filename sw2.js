12a9998ca149c65479523843e1519c5dae7b40c3
rebutnotre <rebutnotre@users.noreply.github.com>
2026-09-12 12:35:08 +0000

    deploy: b19906bf43c8dfde0b3eab7ffbe2eed6a2dd554c

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const cacheName = "1789198604575"; // this gets replaced by the build script
+const cacheName = "1789216500142"; // this gets replaced by the build script
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install", cacheName);
